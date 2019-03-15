db3e5b12f6179732853ba1c3808b2274c1710a5e
Jonathan <jmc@devj.uk>
2019-03-15 16:34:03 +0000

    Meeting Recording & RAS work

---
+++ UnityGameProject/Assets/Scripts/JonathansScripts/RoomAllocationEditor.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(RoomAllocationScript))]
+public class RoomAllocationEditor : Editor
+{
+
+	public override void OnInspectorGUI()
+	{
+		// Not sre what this does really
+		RoomAllocationScript RAS = (RoomAllocationScript)target;
+
+		EditorGUILayout.Space();
+
+
+		EditorGUILayout.LabelField("Room Tracking System");
+
+
+		EditorGUILayout.BeginHorizontal();
+		{
+			for (int i = -1; i < 6; i++)
+			{
+				if (i == -1)
+				{
+					EditorGUILayout.LabelField("People: ", GUILayout.MaxWidth(60f));
+				}
+				else
+				{
+					EditorGUILayout.TextField(RAS.NPCS[i]);
+				}
+			}
+		}
+		EditorGUILayout.EndHorizontal();
+
+		EditorGUILayout.BeginHorizontal();
+		{
+			for (int i = -1; i < 6; i++)
+			{
+				switch (i)
+				{
+					case -1:
+						EditorGUILayout.LabelField("Rooms: ", GUILayout.MaxWidth(60f));
+						break;
+					case 0:
+						EditorGUILayout.EnumPopup(RAS.CallyRoom);
+						break;
+					case 1:
+						EditorGUILayout.EnumPopup(RAS.MirandaRoom);
+						break;
+					case 2:
+						EditorGUILayout.EnumPopup(RAS.PoliceManRoom);
+						break;
+					case 3:
+						EditorGUILayout.EnumPopup(RAS.WitchRoom);
+						break;
+					case 4:
+						EditorGUILayout.EnumPopup(RAS.CatRoom);
+						break;
+					case 5:
+						EditorGUILayout.EnumPopup(RAS.KyleRoom);
+						break;
+					default:
+						break;
+				}
+			}
+		}
+		EditorGUILayout.EndHorizontal();
+	}
+
+
+
+	internal void UpdateEnum(RoomAllocationScript.Room Enum)
+	{
+		
+	}
+}
---
+++ UnityGameProject/Assets/Scripts/JonathansScripts/RoomAllocationScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class RoomAllocationScript : MonoBehaviour
+{
+
+	public const int NumberOfNPCS = 6;
+
+	public List<string> NPCS = new List<string>(new string[]
+		{
+			"Cally", "Miranda", "PoliceMan", "Witch", "Cat", "Kyle"
+		});
+
+	public enum Room
+	{	GrdEnt, GrdStr, GrdA, GrdB, GrdC, GrdD, GrdE, GrdF, GrdG,
+		UppFoy, UppA, UppB, UppC, UppD, UppE, UppF, UppG, UppH, UppI, UppJ, UppK,
+		BseLnd, BseA, BseB, BseC, BseD, BseE, BseF
+	};
+
+	public Room CallyRoom;
+	public Room MirandaRoom;
+	public Room PoliceManRoom;
+	public Room WitchRoom;
+	public Room CatRoom;
+	public Room KyleRoom;
+
+	public string[,] Locations = new string[NumberOfNPCS, 2];
+
+	private void Start()
+	{
+		for (int i = 0; i < NumberOfNPCS; i++)
+		{
+			Locations[0, i] = NPCS[i];
+		}
+
+		for (int i = 0; i < NumberOfNPCS; i++)
+		{
+			Locations[1, i] = Room.GrdEnt.ToString();
+		}
+	}
+}
