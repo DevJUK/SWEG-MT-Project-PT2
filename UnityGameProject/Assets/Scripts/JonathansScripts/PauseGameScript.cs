41f58e13f35181c8b044394bfdad41565f36696a
Jonathan <jmc@devj.uk>
2019-03-28 10:11:04 +0000

    New Tool & Pause Menu Stuff

---
+++ UnityGameProject/Assets/Scripts/JonathansScripts/PauseGameScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGameScript : MonoBehaviour
+{
+	public GameObject PauseUI;
+	private bool IsPaused;
+
+	private void Start()
+	{
+		PauseUI.SetActive(false);
+	}
+
+	void Update()
+    {
+        if (Input.GetButtonDown("Pause") && (!IsPaused))
+		{
+			// Pauses the game
+			Time.timeScale = 0;
+			GetComponent<Mouse_Move>().enabled = false;
+			PauseUI.SetActive(true);
+			IsPaused = true;
+		}
+		else if (Input.GetButtonDown("Pause") && (IsPaused))
+		{
+			// resumes the game
+			Time.timeScale = 1;
+			GetComponent<Mouse_Move>().enabled = true;
+			PauseUI.SetActive(false);
+			IsPaused = false;
+		}
+	}
+
+	public void Menu()
+	{
+		SceneManager.LoadSceneAsync(0);
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
