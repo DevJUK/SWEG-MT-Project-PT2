5855a5e9ad02d33c6af764b608906e98c7ed9c5e
Edward Goodhew <ejgoodhew@btinternet.com>
2019-04-10 17:15:53 +0100

    Upper Foyer event started

---
+++ UnityGameProject/Assets/Scripts/Ed'sScripts/Agile work/UpperFoyerEventScrpt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpperFoyerEventScrpt : MonoBehaviour
+{
+    [Header("Chracter Models")]
+    [Tooltip("The player's model goes here")]
+    public GameObject Player;
+    [Tooltip("The policeman's model goes here")]
+    public GameObject Policeman;
+    [Tooltip("The occultist's model goes here")]
+    public GameObject Occultist;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
