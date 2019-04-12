b9563204436301b4064263a439d8e5b02f58860d
SoItsMaestro <33253668+SoItsMaestro@users.noreply.github.com>
2019-04-12 14:17:11 +0100

    Fucking AI BS

---
+++ UnityGameProject/Assets/Scripts/Samuel'sSuperStuff/FollowScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowScript : MonoBehaviour
+{
+    public Transform FollowObject;
+
+    protected NavMeshAgent Nav;
+    protected Entity This;
+
+
+    // Use this for initialization
+    protected virtual void Start()
+    {
+        This = GetComponent<Entity>();
+        Nav = GetComponent<NavMeshAgent>();        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (FollowObject)
+        {
+            Nav.destination = FollowObject.position;            
+        }
+    }
+}
---
+++ UnityGameProject/Assets/Scripts/Samuel'sSuperStuff/Patrol.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : FollowScript
+{
+    public AINode[] PatrolPoints;
+
+    public int CurrentPoint;
+
+    public bool Reverse;
+
+    public bool Loop;
+
+
+    // Use this for initialization
+    protected override void Start()
+    {
+        base.Start();
+
+        for (int i = 0; i < PatrolPoints.Length; ++i)
+        {
+            PatrolPoints[i].Owner = this;
+            PatrolPoints[i].Ent = This;
+            PatrolPoints[i].NodeNum = i;
+        }
+
+        if (Reverse)
+        {
+            CurrentPoint = PatrolPoints.Length - 1;
+        }
+
+        FollowObject = PatrolPoints[CurrentPoint].transform;
+    }
+
+
+    public void GoToNextPoint()
+    {
+        CurrentPoint += (Reverse) ? -1 : 1;
+
+        if (CurrentPoint > PatrolPoints.Length - 1)
+        {
+            if (Loop)
+            {
+                CurrentPoint = 0;
+            }
+            else
+            {
+                Reverse = true;
+                CurrentPoint = PatrolPoints.Length - 2;
+            }
+        }
+        else if (CurrentPoint < 0)
+        {
+            if (Loop)
+            {
+                CurrentPoint = PatrolPoints.Length - 1;
+            }
+            else
+            {
+                Reverse = false;
+                CurrentPoint = 1;
+            }
+        }
+
+        FollowObject = PatrolPoints[CurrentPoint].transform;
+    }
+   
+}
