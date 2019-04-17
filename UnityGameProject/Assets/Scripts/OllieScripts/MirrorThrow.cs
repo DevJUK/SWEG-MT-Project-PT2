3feb56661fe0971990bba7b5cb95483e66bf66f7
ThatBrummie <oliverneale44@gmail.com>
2019-04-17 13:01:45 +0100

    Mirror Event added to demo

---
+++ UnityGameProject/Assets/Scripts/OllieScripts/MirrorThrow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorThrow : MonoBehaviour
+
+   
+{
+
+    public GameObject MusicBox;
+    private bool ObjectInMirror = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MusicBox.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ObjectInMirror == true)
+        {
+            MusicBox.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Inventory")
+        {
+            Destroy(collision.gameObject);
+            ObjectInMirror = true;
+        }
+          
+    }
+
+}
